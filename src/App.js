78954fb4e959666d4092e4a17b5a63e9d1c57085
Patrycja <patrycja@dynium.ai>
2022-05-26 13:17:03 +0100

    fix: error not found

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import {BrowserRouter, Route, Routes } from 'react-router-dom'
 
 //page components
-
+import Navbar from './components/Navbar'
 import Home from './pages/home/Home'
 import Resources from './pages/resources/Resources'
 import ReactLibraires from './pages/resources/ReactLibraires'
 import Footer from './components/Footer'
 import Collabs from './pages/collabs/Collabs'
 import AboutMe from './pages/aboutme/AboutMe'
+import PageNotFound from './pages/pagenotfound/PageNotFound'
 
 import './App.css'
 
@@ -22,6 +23,7 @@ function App() {
          <Route path="resources/reactlibraries" element={<ReactLibraires/>}/>
          <Route path="collabs" element={<Collabs/>}/>
          <Route path="aboutme" element={<AboutMe/>}/>
+         <Route path="*" element={<PageNotFound/>}/>
        </Routes>
        <Footer/>
        </BrowserRouter>
