1d8310a3f2a6c60149f51ba84d91011729abe1c3
Patrycja <patrycja@dynium.ai>
2022-05-27 10:15:38 +0100

    fix:jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -55,9 +55,9 @@ export default function Create() {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
             </svg>
-          </a>
+          </Link>
         </div>
       </div>
 
@@ -190,7 +190,7 @@ export default function Create() {
           viewBox="0 0 24 24"
           stroke="currentColor"
         >
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
         </svg>
       </Link>
     </div>
--- src/pages/resources/ReactLibraires.jsx
+++ src/pages/resources/ReactLibraires.jsx
@@ -1,61 +1,61 @@
 export default function ReactLibraires() {
   return (
     <div>
-        <section class="text-white bg-gray-900">
-        <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-    <div class="max-w-xl">
-      <h2 class="text-4xl font-bold sm:text-6xl">
+        <section className="text-white bg-gray-900">
+        <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+    <div className="max-w-xl">
+      <h2 className="text-4xl font-bold sm:text-6xl">
         Find what you need! 
       </h2>
 
-      <p class="mt-4 sm:text-2xl">
+      <p className="mt-4 sm:text-2xl">
         Enter to the world of free learning and resources!
       </p>
     </div>
-    <ul class="border border-white sm:grid sm:grid-cols-2 lg:grid-cols-3">
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">50</p>
-        <p class="mt-1 text-xl font-medium">Websites live</p>
-        <details class="p-6 group" open>
-          <summary class="flex items-center justify-between cursor-pointer">
-            <h5 class="text-lg font-medium text-white-900">
+    <ul className="border border-white sm:grid sm:grid-cols-2 lg:grid-cols-3">
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">50</p>
+        <p className="mt-1 text-xl font-medium">Websites live</p>
+        <details className="p-6 group" open>
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h5 className="text-lg font-medium text-white-900">
               Lorem ipsum dolor sit amet consectetur adipisicing?
             </h5>
 
-            <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+            <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
             </span>
           </summary>
 
-          <p class="mt-4 leading-relaxed text-gray-700">
+          <p className="mt-4 leading-relaxed text-gray-700">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
             veritatis molestias culpa in, recusandae laboriosam neque aliquid
             libero nesciunt voluptate dicta quo officiis explicabo consequuntur
@@ -65,75 +65,75 @@ export default function ReactLibraires() {
         
       </li>
 
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">190k+</p>
-        <p class="mt-1 text-xl font-medium">Impressions</p>
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">190k+</p>
+        <p className="mt-1 text-xl font-medium">Impressions</p>
       </li>
 
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">$150k+</p>
-        <p class="mt-1 text-xl font-medium">Client profits</p>
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">$150k+</p>
+        <p className="mt-1 text-xl font-medium">Client profits</p>
       </li>
 
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">10</p>
-        <p class="mt-1 text-xl font-medium">New staff</p>
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">10</p>
+        <p className="mt-1 text-xl font-medium">New staff</p>
       </li>
 
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">1</p>
-        <p class="mt-1 text-xl font-medium">New office</p>
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">1</p>
+        <p className="mt-1 text-xl font-medium">New office</p>
       </li>
 
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">6</p>
-        <p class="mt-1 text-xl font-medium">New tech stacks</p>
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">6</p>
+        <p className="mt-1 text-xl font-medium">New tech stacks</p>
       </li>
     </ul>
-    <ul class="border border-white sm:grid sm:grid-cols-2 lg:grid-cols-3">
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">50</p>
-        <p class="mt-1 text-xl font-medium">Websites live</p>
-        <details class="p-6 group" open>
-          <summary class="flex items-center justify-between cursor-pointer">
-            <h5 class="text-lg font-medium text-white-900">
+    <ul className="border border-white sm:grid sm:grid-cols-2 lg:grid-cols-3">
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">50</p>
+        <p className="mt-1 text-xl font-medium">Websites live</p>
+        <details className="p-6 group" open>
+          <summary className="flex items-center justify-between cursor-pointer">
+            <h5 className="text-lg font-medium text-white-900">
               Lorem ipsum dolor sit amet consectetur adipisicing?
             </h5>
 
-            <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+            <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
+                className="absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
+                className="absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
             </span>
           </summary>
 
-          <p class="mt-4 leading-relaxed text-gray-700">
+          <p className="mt-4 leading-relaxed text-gray-700">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
             veritatis molestias culpa in, recusandae laboriosam neque aliquid
             libero nesciunt voluptate dicta quo officiis explicabo consequuntur
@@ -143,29 +143,29 @@ export default function ReactLibraires() {
         
       </li>
 
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">190k+</p>
-        <p class="mt-1 text-xl font-medium">Impressions</p>
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">190k+</p>
+        <p className="mt-1 text-xl font-medium">Impressions</p>
       </li>
 
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">$150k+</p>
-        <p class="mt-1 text-xl font-medium">Client profits</p>
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">$150k+</p>
+        <p className="mt-1 text-xl font-medium">Client profits</p>
       </li>
 
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">10</p>
-        <p class="mt-1 text-xl font-medium">New staff</p>
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">10</p>
+        <p className="mt-1 text-xl font-medium">New staff</p>
       </li>
 
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">1</p>
-        <p class="mt-1 text-xl font-medium">New office</p>
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">1</p>
+        <p className="mt-1 text-xl font-medium">New office</p>
       </li>
 
-      <li class="p-8 border border-white">
-        <p class="text-3xl font-extrabold">6</p>
-        <p class="mt-1 text-xl font-medium">New tech stacks</p>
+      <li className="p-8 border border-white">
+        <p className="text-3xl font-extrabold">6</p>
+        <p className="mt-1 text-xl font-medium">New tech stacks</p>
       </li>
     </ul>
   </div>
